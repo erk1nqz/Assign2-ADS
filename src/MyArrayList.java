4f0b9fe7031ba20647c8e56acd2fb1fb5d86ba5c
erk1nqz <221326@astanait.edu.kz>
2023-04-13 13:19:21 +0600

    supplement method clear

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -37,7 +37,8 @@ public class MyArrayList<T> implements MyList {
 
     @Override
     public void clear() {
-
+        this.arr = (T[]) new Object[5];
+        this.size = 0;
     }
 
     @Override
