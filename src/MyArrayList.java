ad1ceb62bdbad80d2de534202e2864d9be0f61cc
erk1nqz <221326@astanait.edu.kz>
2023-04-13 13:11:27 +0600

    creating MyArrayList and implement methods

---
+++ src/MyArrayList.java
@@ -0,0 +1,62 @@
+public class MyArrayList<T> implements MyList {
+    private T[] arr;
+    private int size;
+    public MyArrayList() {
+        this.arr = (T[]) new Object[5];
+        this.size = 0;
+    }
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public void add(Object item) {
+
+    }
+
+    @Override
+    public void add(Object item, int index) {
+
+    }
+
+    @Override
+    public boolean remove(Object item) {
+        return false;
+    }
+
+    @Override
+    public Object remove(int index) {
+        return null;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public Object get(int index) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public void sort() {
+
+    }
+}
