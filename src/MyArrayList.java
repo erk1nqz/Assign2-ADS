e2823617ab1db271ff27405555bee4ae1694bcd8
erk1nqz <221326@astanait.edu.kz>
2023-04-19 15:15:42 +0600

    supplement method indexOf

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -91,7 +91,12 @@ public class MyArrayList<E> implements MyList {
 
     @Override
     public int indexOf(Object o) {
-        return 0;
+        for (int i = 0; i < size; i++) {
+            if (arr[i].equals(o)) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     @Override
