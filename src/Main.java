8129599e6e837d0d17538a0f7f0fbb9c8d6b97d5
erk1nqz <221326@astanait.edu.kz>
2023-04-19 22:54:30 +0600

    testing

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
+/*
         MyArrayList list = new MyArrayList();
         list.add(667);
         list.add(228);
@@ -20,6 +20,22 @@ public class Main {
         System.out.println("size now - " + list.size());
         list.remove("aishelek");
         System.out.println("size now - " + list.size());
+*/
 
+        MyLinkedList list = new MyLinkedList();
+        list.add("apple");
+        list.add("banana");
+        list.add("grape");
+        System.out.println("size now - " + list.size());
+        System.out.println(list.contains("banana"));
+        System.out.println("removed - " + list.remove(1));
+        System.out.println("size now - " + list.size());
+        System.out.println(list.contains("banana"));
+        System.out.println(list.get(0));
+        list.add(0, 0);
+        System.out.println(list.get(0));
+        System.out.println(list.indexOf("apple"));
+        list.add("apple");
+        System.out.println(list.lastIndexOf("apple"));
     }
 }
